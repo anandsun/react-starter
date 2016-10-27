fe5029c2df3e84fb89627c0283787b9ece95ca5c
anandsun <anandsun@alumni.upenn.edu>
2016-10-27 18:45:10 -0400

    Update state upon submit
    Made the page display the text that the user typed in below the
    original input when they click submit, otherwise continue displaying
    the old value.

--- src/client/app.js
+++ src/client/app.js
@@ -3,14 +3,49 @@ import ReactDOM from 'react-dom';
 
 import './styles.css';
 
-class HelloWorld extends React.Component {
+class Pokémon extends React.Component {
+  handleSubmit(event) {
+    this.setState({name: this.state.input})
+  }
+
+  handleChange(event) {
+    this.setState({input: event.target.value});
+  }
+
+  // Display the current Pokémon name
+  displayName() {
+    return name_par(this.state.name);
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      name : null,
+      input: null,
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <p>Pokémon:</p>
+        <input type="text" onChange={this.handleChange} />
+        <br />
+        <button onClick={this.handleSubmit}>Submit</button>
+        {this.displayName()}
       </div>
     );
   }
 }
 
-ReactDOM.render(<HelloWorld />, document.getElementById('content'));
+function name_par(name) {
+  return (
+    <p id="name">
+      {name}
+    </p>
+  );
+}
+
+ReactDOM.render(<Pokémon />, document.getElementById('content'));
